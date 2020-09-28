ba13600df695fccf7f79813344f0623d2f1a9066
Susanne Laursen <susanne.laursen@outlook.dk>
2020-09-28 15:44:15 +0200

    Login + menu ongoing

--- app/src/main/java/com/example/deto/SecondActivity.java
+++ app/src/main/java/com/example/deto/SecondActivity.java
@@ -1,8 +1,13 @@
 package com.example.deto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override //sætter menuen til at åbne den rigtige fane af alarm, graf eller vedligehold
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.item2:
+                Intent i = new Intent(SecondActivity.this, Alarm.class);
+            startActivity(i);
+            case R.id.item3:
+                Intent ii = new Intent(SecondActivity.this, GrafiskOversigt.class);
+                startActivity(ii);
+            case R.id.item4:
+                Intent iii = new Intent(SecondActivity.this, KalibreringOgVedligehold.class);
+                startActivity(iii);
+            default:return super.onOptionsItemSelected(item);
+        }
+
     }
 }
