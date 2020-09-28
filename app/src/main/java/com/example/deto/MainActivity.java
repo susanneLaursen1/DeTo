73828727e591792b2243a9eb39311c6fb7e15599
Anisa Hassan <au592172@post.au.dk>
2020-09-28 12:28:58 +0200

    LogInd (Virker ikke endnu)

--- app/src/main/java/com/example/deto/MainActivity.java
+++ app/src/main/java/com/example/deto/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.deto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.editTextTextPassword);
+        Password = (EditText) findViewById(R.id.editTextTextPassword2);
+        Login = (Button)findViewById(R.id.loginBT);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
+    }
+    private void validate(String UserName, String UserPassword){
+        if((UserName == "az44521" )&& (UserPassword == "Efterår2020")){
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        } else{
+            counter--;
+            if(counter == 0){
+                Login.setEnabled(false);
+            }
+
+        }
     }
 }
