9f77741b3c1906a895d0bfaaef5070b549cd55ca
Anisa Hassan <au592172@post.au.dk>
2020-09-29 11:58:05 +0200

    Notification med tal værdi over en bestemt grænse (Virker)

--- app/src/main/java/com/example/deto/MainActivity.java
+++ app/src/main/java/com/example/deto/MainActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText Name;
     private EditText Password;
     private Button Login;
+    private TextView Info;
     private int counter = 5;
+    private TextView Warning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         Name = (EditText)findViewById(R.id.editTextTextPassword);
         Password = (EditText)findViewById(R.id.editTextTextPassword2);
         Login = (Button)findViewById(R.id.loginBT);
+        Info = (TextView)findViewById(R.id.textViewInfo);
+        Warning = (TextView)findViewById(R.id.textWarning);
+
+        Info.setText("Number of attempts remaining: 5");
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,8 +44,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
         } else{
            counter--;
+           Info.setText("Number of attempts remaining: " + String.valueOf(counter));
             if(counter == 0){
                 Login.setEnabled(false);
+                Warning.setText("Try connecting through a computer. You may need to renew your login.");
+               // Toast t = Toast.makeText(MainActivity.this, "Try connecting through a computer. You may need to renew your login.",
+                 //       Toast.LENGTH_LONG);
+                //t.setGravity(0,0,0);
+                //t.show();
             }
 
         }
