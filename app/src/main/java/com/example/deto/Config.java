52db9e44bf750ba0725c6a25c11615f23b107fa3
Kajene <au594051@post.au.dk>
2020-10-08 13:35:43 +0200

    Database forsøg 1

---
+++ app/src/main/java/com/example/deto/Config.java
@@ -0,0 +1,12 @@
+package com.example.deto;
+
+public class Config {
+
+        public static final String DATA_URL = "https://deto-system.000webhostapp.com/InsertData";
+
+        public static final String KEY_NAME = "Name";
+
+        public static final String JSON_ARRAY = "Surname";
+    }
+
+
--- app/src/main/java/com/example/deto/SecondActivity.java
+++ app/src/main/java/com/example/deto/SecondActivity.java
@@ -1,12 +1,8 @@
 package com.example.deto;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,149 +10,147 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
-public class SecondActivity extends AppCompatActivity {
-            private TabLayout tablayout;
-            private ViewPager viewpager;
-            private TabItem tab1, tab2, tab3;
-            public PageAdapter pagerAdapter;
-            DatabaseHelper myDb;
-            EditText editname,editsurname,editdate,editnitrit;
-            Button button;
-            Button button2;
-            Button button3;
-            Button button4;
+import java.util.HashMap;
 
+
+public class SecondActivity extends AppCompatActivity {
+    private TabLayout tablayout;
+    private ViewPager viewpager;
+    private TabItem tab1, tab2, tab3;
+    public PageAdapter pagerAdapter;
+    EditText name, surname, date, nitritvalue;
+    Button insertdata;
+    ProgressDialog mProgressDialog;
+    //private static final String INSERTDATA_URL = "https://deto-system.000webhostapp.com/InsertData/add_data.php";
+    String ServerURL = "https://deto-system.000webhostapp.com/InsertData/add_data.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        myDb = new DatabaseHelper(this);
-        editname = (EditText)findViewById(R.id.text);
-        editsurname = (EditText)findViewById(R.id.text2);
-        editdate = (EditText)findViewById(R.id.text3);
-        editnitrit = (EditText)findViewById(R.id.text4);
-        button=(Button)findViewById(R.id.button);
-        button2=(Button)findViewById(R.id.button2);
-        button3=(Button)findViewById(R.id.button3);
-        button4=(Button)findViewById(R.id.button4);
-        AddData();
-        viewAll();
-        UpdateData();
-        DeleteData();
+
+        name = (EditText) findViewById(R.id.editTextname);
+        surname = (EditText) findViewById(R.id.editTextsurname);
+        date = (EditText) findViewById(R.id.editTextdate);
+        nitritvalue = (EditText) findViewById(R.id.editTextnitrit);
+        insertdata = (Button) findViewById(R.id.buttoninsertdata);
+
+
+        insertdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Name = name.getText().toString().trim().toLowerCase();
+                String Surname = surname.getText().toString().trim().toLowerCase();
+                String Date = date.getText().toString().trim().toLowerCase();
+                String Nitritvalue = nitritvalue.getText().toString().trim().toLowerCase();
+
+                if (Name.equals("")||Surname.equals("")||Date.equals("")||Nitritvalue.equals("")){
+                    Toast.makeText(SecondActivity.this, "Please Enter Detail", Toast.LENGTH_SHORT).show();
+                }
+                InsertData();
+
+            }
+        });
 
     }
 
+    private void InsertData() {
+
+        String Name = name.getText().toString().trim().toLowerCase();
+        String Surname = surname.getText().toString().trim().toLowerCase();
+        String Date = date.getText().toString().trim().toLowerCase();
+        String Nitritvalue = nitritvalue.getText().toString().trim().toUpperCase();
+
+        if (Name.equals("") || Surname.equals("") || Date.equals("") || Nitritvalue.equals("")) {
+            Toast.makeText(SecondActivity.this, "Please Enter Detail", Toast.LENGTH_SHORT).show();
+        } else {
 
 
-        public void DeleteData() {
-            button4.setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Integer deletedRows = myDb.deleteData(editname.getText().toString());
-                            if (deletedRows > 0)
-                                Toast.makeText(SecondActivity.this, "Data Deleted", Toast.LENGTH_LONG).show();
-                            else
-                                Toast.makeText(SecondActivity.this, "Data Not Deleted", Toast.LENGTH_LONG).show();
+            register(Name, Surname, Date, Nitritvalue);
+        }
+    }
 
-                        }
-                    }
-            );
+    private void register(String Name, String Surname, String Date, String Nitritvalue) {
+        class RegisterUsers extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+            RegisterUserClasses ruc = new RegisterUserClasses();
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                mProgressDialog = new ProgressDialog(SecondActivity.this);
+                mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                mProgressDialog.setMessage("Please wait...");
+                mProgressDialog.setIndeterminate(true);
+                mProgressDialog.setCancelable(false);
+                mProgressDialog.setProgress(0);
+                mProgressDialog.setProgressNumberFormat(null);
+                mProgressDialog.setProgressPercentFormat(null);
+                mProgressDialog.show();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                mProgressDialog.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(SecondActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap<String, String>();
+
+
+                data.put("Name", params[0]);
+                data.put("Surname", params[1]);
+                data.put("Date", params[2]);
+                data.put("Nitritvalue", params[3]);
+
+
+                String result = ruc.sendPostRequest(ServerURL, data);
+
+                return result;
+            }
         }
 
+        RegisterUsers ru = new RegisterUsers();
+        ru.execute(Name, Surname, Date, Nitritvalue);
+    }
 
-    public void UpdateData(){
-        button3.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        boolean isUpdate = myDb.updateData(editname.getText().toString(),editsurname.getText().toString()
-                                ,editdate.getText().toString(),editnitrit.getText().toString() );
-                        if(isUpdate==true)
-                            Toast.makeText(SecondActivity.this, "Data Updated", Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(SecondActivity.this, "Data Not Updated", Toast.LENGTH_LONG).show();
 
-                    }
+
+            @Override //sætter menuen til at åbne den rigtige fane af alarm, graf eller vedligehold
+            public boolean onOptionsItemSelected (@NonNull MenuItem item){
+                switch (item.getItemId()) {
+
+                    case R.id.item2:
+                        Intent i = new Intent(SecondActivity.this, Alarm.class);
+                        startActivity(i);
+                        return true;
+                    case R.id.item3:
+                        Intent ii = new Intent(SecondActivity.this, GrafiskOversigt.class);
+                        startActivity(ii);
+                        return true;
+                    case R.id.item4:
+                        Intent iii = new Intent(SecondActivity.this, KalibreringOgVedligehold.class);
+                        startActivity(iii);
+                        return true;
+                    default:
+                        return super.onOptionsItemSelected(item);
                 }
-        );
-    }
-    public void AddData(){
-        button.setOnClickListener(
-                new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        boolean isInserted = myDb.insertData(editname.getText().toString(),editsurname.getText().toString()
-                                ,editdate.getText().toString(),editnitrit.getText().toString() );
-                        if(isInserted==true)
-                            Toast.makeText(SecondActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(SecondActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
 
-                    }
-                }
-        );
-
-    }
-
-    public void viewAll(){
-        button2.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Cursor res = myDb.getAllData();
-                        if(res.getCount() == 0){
-                            //show message
-                            showMessage("Error","Nothing found");
-                            return;
-                        }
-                        StringBuffer buffer = new StringBuffer();
-                        while (res.moveToNext()){
-                            buffer.append("Name: "+ res.getString(0) + "\n");
-                            buffer.append("Surname: "+ res.getString(1) + "\n");
-                            buffer.append("Date: "+ res.getString(2) + "\n");
-                            buffer.append("Nitrit value: "+ res.getString(3) + "\n\n");
-                        }
-                        showMessage("Data",buffer.toString());
-                    }
-                }
-        );
-    }
-
-            public void showMessage(String title, String Message){
-                  AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                  builder.setCancelable(true);
-                  builder.setTitle(title);
-                  builder.setMessage(Message);
-                  builder.show();
-    }
-
-
-
-
-    @Override //sætter menuen til at åbne den rigtige fane af alarm, graf eller vedligehold
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-
-            case R.id.item2:
-                Intent i = new Intent(SecondActivity.this, Alarm.class);
-            startActivity(i);
-            return true;
-            case R.id.item3:
-                Intent ii = new Intent(SecondActivity.this, GrafiskOversigt.class);
-                startActivity(ii);
-                return true;
-            case R.id.item4:
-                Intent iii = new Intent(SecondActivity.this, KalibreringOgVedligehold.class);
-                startActivity(iii);
-                return true;
-            default:return super.onOptionsItemSelected(item);
+            }
         }
 
-    }
-}
